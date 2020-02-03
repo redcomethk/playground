fad4fa0eeb09615feec3e226e06bde7e72aa3d07
Ralph CHAN <chan.wai.fu.ralph@gmail.com>
2020-02-03 19:09:18 +0800

    Support remote connection for redis

--- routes/queue.js
+++ routes/queue.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+const { REDIS_STRING, REDIS_PASSWORD, REDIS_HOST, REDIS_PORT } = process.env;
+console.log(REDIS_STRING)
+
 const path = require('path');
 const Queue = require('bull');
-const queue = new Queue('TEST:BULL_SEPARATE');
+const queue = new Queue('TEST:BULL_SEPARATE', {
+  // do not pass redis url in constructor if supplying ioredis options directly:
+  redis: {
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+    db: 0,
+    tls: {},
+    password: REDIS_PASSWORD,
+    connectTimeout: 30000
+  },
+  // ... another Queue options ...
+});
 queue.process(1, path.join(__dirname, '../', 'queues/test.js'));
 const { setQueues } = require('bull-board');
 let queues = [
